9d3f73ee670988ecc609554268a9a4c737274136
Matheo <matheosantana30@gmail.com>
2024-08-31 15:08:18 -0300

    ms Crear dto entrada turno

---
+++ src/main/java/com/backend/clinicaOdontologica/dto/entrada/TurnoEntradaDto.java
@@ -0,0 +1,54 @@
+package com.backend.clinicaOdontologica.dto.entrada;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TurnoEntradaDto {
+
+    @NotNull(message = "Debe ingresar un odontologo")
+    private Long pacienteId;
+
+    @NotNull(message = "Debe ingresar un odontologo")
+    private Long odontologoId;
+
+    @FutureOrPresent(message = "La fecha no puede ser anterior al día de hoy")
+    @NotNull(message = "Debe especificarse la fecha del turno")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDateTime fechaHora;
+
+    public TurnoEntradaDto() {}
+
+    public TurnoEntradaDto(Long pacienteId, Long odontologoId, LocalDateTime fechaHora) {
+        this.pacienteId = pacienteId;
+        this.odontologoId = odontologoId;
+        this.fechaHora = fechaHora;
+    }
+
+    public Long getPacienteId() {
+        return pacienteId;
+    }
+
+    public void setPacienteId(Long pacienteId) {
+        this.pacienteId = pacienteId;
+    }
+
+    public Long getOdontologoId() {
+        return odontologoId;
+    }
+
+    public void setOdontologoId(Long odontologoId) {
+        this.odontologoId = odontologoId;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+}
