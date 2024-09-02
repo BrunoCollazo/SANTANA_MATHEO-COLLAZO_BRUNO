40582254d9373a46ccb6b968c03ac5f74fceb522
Matheo <matheosantana30@gmail.com>
2024-09-02 19:46:53 -0300

    ms Crear service de Turno

--- src/main/java/com/backend/clinicaOdontologica/service/impl/TurnoService.java
+++ src/main/java/com/backend/clinicaOdontologica/service/impl/TurnoService.java
@@ -1,8 +1,13 @@
 package com.backend.clinicaOdontologica.service.impl;
 
 import com.backend.clinicaOdontologica.dto.entrada.TurnoEntradaDto;
+import com.backend.clinicaOdontologica.dto.salida.PacienteSalidaDto;
 import com.backend.clinicaOdontologica.dto.salida.TurnoSalidaDto;
+import com.backend.clinicaOdontologica.entity.Odontologo;
+import com.backend.clinicaOdontologica.entity.Paciente;
 import com.backend.clinicaOdontologica.entity.Turno;
+import com.backend.clinicaOdontologica.repository.OdontologoRepository;
+import com.backend.clinicaOdontologica.repository.PacienteRepository;
 import com.backend.clinicaOdontologica.repository.TurnoRepository;
 import com.backend.clinicaOdontologica.service.ITurnoService;
 import com.backend.clinicaOdontologica.utils.JsonPrinter;
@@ -16,29 +21,62 @@ public class TurnoService implements ITurnoService {
 
     private final Logger LOGGER = LoggerFactory.getLogger(PacienteService.class);
     private final TurnoRepository turnoRepository;
+    private final PacienteRepository pacienteRepository;
+    private final OdontologoRepository odontologoRepository;
     private final ModelMapper modelMapper;
 
-    public TurnoService(TurnoRepository turnoRepository, ModelMapper modelMapper) {
+    public TurnoService(TurnoRepository turnoRepository, PacienteRepository pacienteRepository, OdontologoRepository odontologoRepository, ModelMapper modelMapper) {
         this.turnoRepository = turnoRepository;
+        this.pacienteRepository = pacienteRepository;
+        this.odontologoRepository = odontologoRepository;
         this.modelMapper = modelMapper;
         configureMapping();
     }
 
     @Override
     public TurnoSalidaDto registrarTurno(TurnoEntradaDto turno) {
-//        Turno entidadTurno = modelMapper.map(turno, Turno.class);
-//
-//        Turno turnoRegistrado = turnoRepository.save(entidadTurno);
-//
-//        TurnoSalidaDto turnoSalidaDto = modelMapper.map(turnoRegistrado, TurnoSalidaDto.class);
-//
-//        return turnoSalidaDto;
-        return null;
+        LOGGER.info("TurnoEntradaDto: {}", JsonPrinter.toString(turno));
+
+        TurnoSalidaDto turnoSalidaDto = null;
+
+        Odontologo odontologo = odontologoRepository.findById(turno.getOdontologoId())
+                .orElse(null);
+
+        Paciente paciente = pacienteRepository.findById(turno.getPacienteId())
+                .orElse(null);
+        if(paciente == null && odontologo == null){
+            LOGGER.error("No existen ni el odontologo "+ turno.getOdontologoId()+" ni el paciente " + turno.getPacienteId());
+        }else if (paciente == null){
+            LOGGER.error("No existe el paciente " + turno.getPacienteId());
+        }else if (odontologo == null){
+            LOGGER.error("No existe el odontologo "+ turno.getOdontologoId());
+        }else{
+
+            Turno entidadTurno = modelMapper.map(turno, Turno.class);
+            LOGGER.info("entidadTurno: {}", JsonPrinter.toString(entidadTurno));
+
+            entidadTurno.setOdontologo(odontologo);
+            entidadTurno.setPaciente(paciente);
+
+            Turno turnoRegistrado = turnoRepository.save(entidadTurno);
+            LOGGER.info("turnoRegistrado: {}", JsonPrinter.toString(turnoRegistrado));
+
+            turnoSalidaDto = modelMapper.map(turnoRegistrado, TurnoSalidaDto.class);
+
+        }
+        LOGGER.info("turnoSalidaDto: {}", JsonPrinter.toString(turnoSalidaDto));
+        return turnoSalidaDto;
     }
 
     @Override
     public List<TurnoSalidaDto> listarTurnos() {
-        return null;
+        List<TurnoSalidaDto> turnoSalidaDtos = turnoRepository.findAll()
+                .stream()
+                .map(turno -> modelMapper.map(turno, TurnoSalidaDto.class))
+                .toList();
+        LOGGER.info("Listado de todos los turnos: {}", JsonPrinter.toString(turnoSalidaDtos));
+
+        return turnoSalidaDtos;
     }
     private void configureMapping(){
         modelMapper.typeMap(TurnoEntradaDto.class, Turno.class);
